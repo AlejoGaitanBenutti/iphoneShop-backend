1715e0748b32bf2b3330c9018f4feca7275e9c47
AlejoGaitanBenutti <alejogbs8@gmail.com>
2025-06-13 18:16:24 -0300

    fix en listar usuarios

--- auth/login.php
+++ auth/login.php
@@ -1,9 +1,9 @@
 <?php
 
-error_log("🔍 Se ejecutó login.php");
-
-require_once __DIR__ . '/../utils/init.php';
 require_once __DIR__ . "/../utils/cors.php";
+error_log("🔍 Se ejecutó login.php");
+require_once __DIR__ . '/../utils/init.php';
+
 
 if ($_SERVER["REQUEST_METHOD"] !== "POST") {
     http_response_code(405);
